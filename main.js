9149774296bfa6ccdfe3254027aeb5d14726ef72
Thomas Denney <me@thomasdenney.co.uk>
2016-09-20 10:34:13 +0100

    Load correct URL with local token

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ const path = require('path')
 let win
 
 function createWindow () {
-
   pxt.mainCli(path.join(process.cwd(), "node_modules/pxt-microbit"), ["serve", "-no-browser"])
   win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL(`file://${__dirname}/index.html`)
+  win.loadURL(`file://${__dirname}/index.html#local-token=${pxt.globalConfig.localToken}`)
   win.on('closed', () => {
     win = null
   })
