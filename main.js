b9ee6f7426ea24833c080c20c119d4597b4a1d99
lucas <lucas.shao@foxmail.com>
2018-05-16 18:36:08 +0800

    add electron package config.

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-const {app, BrowserWindow, Menu} = require('electron')
-const pxt = require('pxt-core')
-const path = require('path')
+const {app, BrowserWindow, Menu} = require('electron');
+const pxt = require('pxt-core');
+const path = require('path');
 
-let win
+let win;
 
-const cliPath = path.join(process.cwd(), "node_modules/pxt-microbit") 
+const cliPath = path.join(process.cwd(), "node_modules/pxt-microbit");
 
 function startServerAndCreateWindow() {
-  pxt.mainCli(cliPath, ["serve", "-no-browser"])
-  createWindow()
+  pxt.mainCli(cliPath, ["serve", "-no-browser"]);
+  createWindow();
 }
 
-function createWindow () {
+function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
     title: "code the micro:bit"
-  })
-  Menu.setApplicationMenu(null)
-  win.loadURL(`file://${__dirname}/index.html#local_token=${pxt.globalConfig.localToken}`)
+  });
+  Menu.setApplicationMenu(null);
+  win.loadURL(`file://${__dirname}/index.html#local_token=${pxt.globalConfig.localToken}`);
   win.on('closed', () => {
     win = null
   })
 }
 
-app.on('ready', startServerAndCreateWindow)
+app.on('ready', startServerAndCreateWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
-})
+});
