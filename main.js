e6b471c3bf93a871e3d93a9a311c49ffc8955903
lucas <lucas.shao@foxmail.com>
2018-06-04 11:07:19 +0800

    remove unnecessary index.html file.

--- main.js
+++ main.js
@@ -1,61 +1,51 @@
 var _a = require("electron"), app = _a.app, BrowserWindow = _a.BrowserWindow, Menu = _a.Menu, dialog = _a.dialog;
-var pxt = require("pxt-core");
 var path = require("path");
-var Thenjs = require("thenjs");
 var mainWindow;
 var isWindows = process.platform === "win32";
 function printCurrentTime(msg) {
     console.log((new Date()).toJSON().slice(0, 24).replace(/[-T]/g, ":"), msg);
 }
-function main() {
-    printCurrentTime("start main function.");
-    var cliPath = path.join(__dirname, "node_modules/pxt-microbit");
-    pxt.mainCli(cliPath, ["serve", "-no-browser"]);
-    printCurrentTime("end main function");
-}
-Thenjs(main()).then(function () {
-    app.on("ready", function () {
-        printCurrentTime("start create window");
-        mainWindow = new BrowserWindow({
-            title: "Magibit micro:bit",
-            show: false,
-            webPreferences: {
-                nodeIntegrationInWorker: true
-            }
-        });
-        mainWindow.loadURL(path.join("file://", __dirname, "index.html#local_token=" + pxt.globalConfig.localToken));
-        mainWindow.on("ready-to-show", function () {
-            mainWindow.show();
-        });
-        mainWindow.on("closed", function () {
-            mainWindow = null;
-        });
-        var menu = Menu.buildFromTemplate([
-            {
-                label: isWindows ? "File" : app.getName(),
-                submenu: [
-                    {
-                        label: isWindows ? "Exit" : "Quit " + app.getName(),
-                        accelerator: isWindows ? null : "CmdOrCtrl+Q",
-                        click: function () {
-                            app.quit();
-                        }
-                    },
-                    {
-                        label: "Say Hello",
-                        click: function () {
-                            dialog.showMessageBox(mainWindow, {
-                                type: "info",
-                                message: "Hello",
-                                detail: "Just a friendly meow.",
-                                buttons: ["Meow", "Close"],
-                                defaultId: 0
-                            });
-                        }
-                    }
-                ]
-            },
-        ]);
-        Menu.setApplicationMenu(menu);
+app.on("ready", function () {
+    printCurrentTime("start create window");
+    mainWindow = new BrowserWindow({
+        title: "Magibit micro:bit",
+        show: false,
+        webPreferences: {
+            nodeIntegrationInWorker: true
+        }
     });
+    mainWindow.loadURL(path.join("file://", __dirname, "index.html"));
+    mainWindow.on("ready-to-show", function () {
+        mainWindow.show();
+    });
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
+    var menu = Menu.buildFromTemplate([
+        {
+            label: isWindows ? "File" : app.getName(),
+            submenu: [
+                {
+                    label: isWindows ? "Exit" : "Quit " + app.getName(),
+                    accelerator: isWindows ? null : "CmdOrCtrl+Q",
+                    click: function () {
+                        app.quit();
+                    }
+                },
+                {
+                    label: "Say Hello",
+                    click: function () {
+                        dialog.showMessageBox(mainWindow, {
+                            type: "info",
+                            message: "Hello",
+                            detail: "Just a friendly meow.",
+                            buttons: ["Meow", "Close"],
+                            defaultId: 0
+                        });
+                    }
+                }
+            ]
+        },
+    ]);
+    Menu.setApplicationMenu(menu);
 });
--- main.ts
+++ main.ts
@@ -1,68 +1,26 @@
 const {app, BrowserWindow, Menu, dialog} = require("electron");
-const pxt = require("pxt-core");
 const path = require("path");
-const Thenjs = require("thenjs");
+const http = require("http");
 
 let mainWindow: any;
-const isWindows = process.platform === "win32";
 
-function printCurrentTime(msg: string) {
-  console.log((new Date()).toJSON().slice(0, 24).replace(/[-T]/g, ":"), msg);
-}
+const appRootPath = 'app-packaged';
 
-function main() {
-  printCurrentTime("start main function.");
-  const cliPath = path.join(__dirname, "node_modules/pxt-microbit");
-  pxt.mainCli(cliPath, ["serve", "-no-browser"]);
-  printCurrentTime("end main function");
-}
+http.createServer();
 
-Thenjs(main()).then(function () {
-  app.on("ready", () => {
-    printCurrentTime("start create window");
-    mainWindow = new BrowserWindow({
-      title: "Magibit micro:bit",
-      show: false,
-      webPreferences: {
-        nodeIntegrationInWorker: true
-      }
-    });
+mainWindow = new BrowserWindow({
+  title: "Magibit micro:bit",
+  show: false,
+  webPreferences: {
+    nodeIntegrationInWorker: true
+  }
+});
 
-    mainWindow.loadURL(path.join("file://", __dirname, `index.html#local_token=${pxt.globalConfig.localToken}`));
-    mainWindow.on("ready-to-show", () => {
-      mainWindow.show();
-    });
+mainWindow.loadURL(path.join("file://", appRootPath, `index.html`));
+mainWindow.on("ready-to-show", () => {
+  mainWindow.show();
+});
 
-    mainWindow.on("closed", () => {
-      mainWindow = null;
-    });
-
-    const menu = Menu.buildFromTemplate([
-      {
-        label: isWindows ? "File" : app.getName(),
-        submenu: [
-          {
-            label: isWindows ? "Exit" : `Quit ${app.getName()}`,
-            accelerator: isWindows ? null : "CmdOrCtrl+Q",
-            click() {
-              app.quit();
-            }
-          },
-          {
-            label: "Say Hello",
-            click() {
-              dialog.showMessageBox(mainWindow, {
-                type: "info",
-                message: "Hello",
-                detail: "Just a friendly meow.",
-                buttons: ["Meow", "Close"], //can pass multiple buttons in here and then get the index of the clicked on in the callback
-                defaultId: 0
-              });
-            }
-          }
-        ]
-      },
-    ]);
-    Menu.setApplicationMenu(menu);
-  });
+mainWindow.on("closed", () => {
+  mainWindow = null;
 });
